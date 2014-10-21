007fb89f07aeec5248b817d534f4fa2fed2e308b
NicolePell <pellicena@gmail.com>
2014-10-21 14:55:38 +0100

    Added choice descriptions

--- public/js/game.js
+++ public/js/game.js
@@ -2,25 +2,33 @@
 
   		var game = new Rules
   		var gest = {rock: new Rock, paper: new Paper, scissors: new Scissors, lizard: new Lizard, spock: new Spock}
-  		var computersChoice = function() {
-  			gestures = [new Rock, new Paper, new Scissors, new Lizard, new Spock];
-  			return gestures[Math.floor(Math.random()* gestures.length)];
-  		};
+      var computersChoice =  function() {
+  		  gestures = [new Rock, new Paper, new Scissors, new Lizard, new Spock];
+        return gestures[Math.floor(Math.random()* gestures.length)];
+
+      };
+  	
+
+
+      var playersChoice = function(choice) {
+        return gest[$(choice).data('gesture')]
+      };
 
   		var andTheWinnerIs = function(winner) {
-        if(winner === 'Draw')
-             $('h1').text("Snap, it's a " + winner);
-           
+        if(winner.type === 'Draw')
+             $('h2').text("Snap, it's a " + winner.type);
+              
         else
-  			     $('h1').text("The winner is " + winner);
-  		};
-
-  		var playersChoice = function(choice) {
-  			return gest[$(choice).data('gesture')]
+  			     $('h2').text("The winner is " + winner.type);
+              
   		};
 
   		$('.img-choice').on('click', function() {
-  			andTheWinnerIs(game.winner(playersChoice(this), computersChoice()).type)
+        var compChoice = computersChoice();
+  			andTheWinnerIs(game.winner(playersChoice(this), compChoice))
+        $('h3').text("Computer chose " + compChoice.type + " " + "Player chose " + playersChoice(this).type)
   		});
+
+
 
 });
