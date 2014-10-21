120e4adf31481b670d88ceede9f20d6d4e978d35
slstevens <sean.stevens.123@gmail.com>
2014-10-21 17:53:12 +0100

    Made it more amazing

--- public/js/game.js
+++ public/js/game.js
@@ -1,5 +1,6 @@
   	$(document).ready(function() {
       var compChoice;
+      var playChoice;
   		var game = new Rules
   		var gest = {rock: new Rock, paper: new Paper, scissors: new Scissors, lizard: new Lizard, spock: new Spock}
       var computersChoice =  function() {
@@ -16,15 +17,17 @@
 
   		var andTheWinnerIs = function(winner) {
         if(winner.type === 'Draw')
-             $('#result').text("Snap, it's a " + winner.type + "! " + "You both chose "+ compChoice.type + ". Choose again!");      
-        else
-  			     $('#result').text("The winner is " + winner.type);
+            $('#result').text("Snap, it's a " + winner.type + "! " + "You both chose "+ compChoice.type + ". Choose again!");      
+        if (winner.type === playChoice.type)
+            $('#result').text("You win! Your " + playChoice.type.toLowerCase() + " beat your opponent's " + compChoice.type.toLowerCase())
+        if (winner.type === compChoice.type)
+            $('#result').text("You lose! Your " + playChoice.type.toLowerCase() + " lost against your opponent's " + compChoice.type.toLowerCase())
   		};
 
   		$('.img-choice').on('click', function() {
         compChoice = computersChoice();
-  			andTheWinnerIs(game.winner(playersChoice(this), compChoice))
-        $('#choices').text("Computer chose " + compChoice.type + " " + ". You chose " + playersChoice(this).type)
+        playChoice = playersChoice(this);
+  			andTheWinnerIs(game.winner(playChoice, compChoice));
   		});
 
 });
